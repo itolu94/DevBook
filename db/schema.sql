a8a9602c2819675709976b211fe5603e386c4a32
Tolu Idowu <itolu94@gmail.com>
2017-06-10 10:36:54 -0400

    init

--- db/schema.sql
+++ db/schema.sql
@@ -3,16 +3,6 @@ USE jrdevbook_db;
 
 DROP TABLE IF EXISTS members;
 
-CREATE TABLE userInfo (
-id int NOT NULL AUTO_INCREMENT,
-first_name varchar(255) NOT NULL,
-last_name varchar(255) NOT NULL,
-email varchar(255) NOT NULL,
-birthday DATE NOT NULL,
-signup_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-PRIMARY KEY (id)
-);
-
 CREATE TABLE memberInfo (
 id int NOT NULL AUTO_INCREMENT,
 username varchar(255) NOT NULL,
@@ -20,15 +10,26 @@ password varchar(255) NOT NULL,
 PRIMARY KEY (id)
 );
 
+CREATE TABLE userInfo (
+user_id int NOT NULL,
+first_name varchar(255) NOT NULL,
+last_name varchar(255) NOT NULL,
+email varchar(255) NOT NULL,
+birthday DATE NOT NULL,
+signup_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+);
+
+
 CREATE TABLE postInfo (
-post varchar(255) NOT NULL
+id INT NOT NULL AUTO_INCREMENT,
+post varchar(255) NOT NULL,
 date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
 authorId int NOT NULL,
-PRIMARY KEY (authorId)
+PRIMARY KEY (id)
 );
 
 CREATE TABLE comments (
-comments varchar(355) NOT NULL
+comments varchar(355) NOT NULL,
 date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
 commentId int NOT NULL,
 PRIMARY KEY (commentId)
